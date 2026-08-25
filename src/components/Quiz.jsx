98b7ba4bb84b0bfa26838f8af833a306fe76a26e
Uy <leuy091104@gmail.com>
2026-08-25 23:44:55 +0700

    refactor: shuffle the answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -16,6 +16,17 @@ export default function Quiz() {
   // biến chứa index của câu hỏi tính bằng độ dài của mảng câu trả lời
   // mảng chứa 2 câu trả lời => index = 2 => câu hỏi thứ 3
   const activeQuestionIndex = userAnswers.length;
+  // biến chứa các câu trả lời của câu hỏi hiện tại để thực hiện xáo trộn các câu trả lời
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  // thực xáo trộn các câu trả lời của câu hỏi hiện tại và trả về chính array đó
+  // sort() cần 1 function trả về:
+  // <0 => đổi thứ tự
+  // >0 => giữ/đảo theo quy tắc sort
+  // =0 => coi như bằng nhau
+  // trong khi đó,
+  // Math.random() sẽ trả về số thực ngẫu nhiên >=0 và <1
+  // Math.random() - 0.5 sẽ trả về số thực ngẫu nhiên >=-0.5 và <0.5, tức là có thể âm, dương hoặc =0
+  shuffledAnswer.sort(() => Math.random() - 0.5);
   // Hàm xử lý chọn câu trả lời
   function handleSelectAnswer(selectedAnswer) {
     // Cập nhật mảng các câu trả lời dựa vào state trước đó và thêm câu trả lời mới vào mảng
@@ -31,7 +42,7 @@ export default function Quiz() {
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         {/* Danh sách các câu trả lời */}
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswer.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
